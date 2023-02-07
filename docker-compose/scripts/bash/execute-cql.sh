a16372c28b59ed3ac1565efb3bad3e124792adca
ivan.polovyi <polovyivan@yahoo.com>
2023-02-06 22:36:31 -0300

    working version

--- docker-compose/scripts/bash/execute-cql.sh
+++ docker-compose/scripts/bash/execute-cql.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
-echo "Executing scripts"
+echo "########################Starting to execute SH script...########################"
+USER_NAME='cassandra'
+PASSWORD='cassandra'
 
-#nodetool disablegossip
-#nodetool disablebinary
-#nodetool drain
-#kill 'cat cassandra.pid'
-
-while ! cqlsh cassandra -u 'cassandra' -p 'cassandra' -e 'describe cluster' ; do
+while ! cqlsh cassandra_db -u "${USER_NAME}" -p "${PASSWORD}" -e 'describe cluster' ; do
+     echo "########################Waiting for main instance to be ready...########################"
      sleep 1
 done
 
-for cql_file in ./tmp/cql/ax/*.cql;
+for cql_file in ./tmp/cql/*.cql;
 do
-  if !  grep -q "USE ax;" "${cql_file}"; then
-     sed -i '1s/^/USE ax; /' "${cql_file}";
-  fi
-  cqlsh cassandra -u 'cassandra' -p 'cassandra' -f "${cql_file}" ;
-  echo "########################Script ""${cql_file}"" executed!!!!!!!!!!!!!!!!!!!!!!"
+  cqlsh cassandra_db -u "${USER_NAME}" -p "${PASSWORD}" -f "${cql_file}" ;
+  echo "########################Script ""${cql_file}"" executed!!!########################"
 done
-echo "Script execution finished"
-
-
-
-#cqlsh cassandra -u 'cassandra' -p 'cassandra';
-#Describe keyspaces;
-
-#cat ./tmp/cql/ax*.cql | cqlsh cassandra -u 'cassandra' -p 'cassandra'
-#for cql_file in ls ./tmp/cql/ax/*.cql; do cqlsh cassandra -u 'cassandra' -p 'cassandra' -f "${cql_file}" ; done
-
-  # sed -i '1s/^/USE ax; /' "${cql_file}";
-  #grep -qxF 'USE ax;'"${cql_file}"'"' foo.bar || sed -i '1s/^/USE ax; /' "${cql_file}";
-# "USE ax;" "${cql_file}" || sed -i '1s/^/USE ax; /' "${cql_file}";
+echo "########################Execution of SH script is finished!########################"
+echo "########################Stopping temporary instance!########################"
